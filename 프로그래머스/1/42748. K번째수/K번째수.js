021d413b0b60a441a89159c3ed0149ab13f1d4a8
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-05-28 10:58:13 +0900

    [level 1] Title: K번째수, Time: 5.69 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,7 +1,8 @@
 function solution(array, commands) {
     var answer = [];
-    for(let i = 0; i <commands.length; i++){
-       answer.push(array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b)[commands[i][2]-1])
+    for(let i = 0; i < commands.length; i++){
+        answer.push(array.slice(commands[i][0]-1, commands[i][1]).sort((a,b)=>a-b)[commands[i][2]-1])
+        console.log(answer)
     }
     return answer;
 }
