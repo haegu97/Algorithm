83a94e74c5bdd5e7fdb8758d66ea394da31ef2f0
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-05-28 13:57:15 +0900

    [level 1] Title: 최소직사각형, Time: 14.45 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,17 +1,13 @@
 function solution(sizes) {
-    
-    let a = 0
-    let b = 0
-    
+    var answer = 0;
+    const arr1 = []
+    const arr2 = []
     for(let i = 0; i < sizes.length; i++){
-        sizes[i].sort((a, b)=> a-b)
-        if (sizes[i][0] > a){
-            a = sizes[i][0]
-        }
-        if (sizes[i][1] > b){
-            b = sizes[i][1]
-        }
+        sizes[i].sort((a,b)=>b-a)
+        arr1.push(sizes[i][0])
+        arr2.push(sizes[i][1])
     }
-    
-    return a*b;
+    arr1.sort((a,b)=>b-a)
+    arr2.sort((a,b)=>b-a)
+    return arr1[0] * arr2[0]
 }
