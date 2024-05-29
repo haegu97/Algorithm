f6df8a81fbed008d32cf046500205d264763d9c0
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-05-29 16:12:57 +0900

    [level 1] Title: 폰켓몬, Time: 3.36 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,15 +1,7 @@
 function solution(nums) {
-    var answer = 0;
-    const arr = []
-    const max = nums.length / 2
-    for(let i = 0; i < nums.length; i++){
-        if(arr.length < max){
-            if(!arr.includes(nums[i])){
-            arr.push(nums[i])
-            
-        }
-        }
-        
-    }
-    return arr.length;
+    let myBag = [...new Set(nums)];
+    let limit = nums.length / 2;
+    console.log(myBag)
+    
+    return myBag.length > limit ? limit : myBag.length;
 }
