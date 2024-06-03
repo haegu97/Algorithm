359c1f7de16450133e77dfa1a5e9eb7049fbe12a
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-06-03 20:09:25 +0900

    [level 2] Title: H-Index, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,10 +1,9 @@
 function solution(citations) {
-    citations.sort((a, b) => b - a)
-    for(let i = 0; i < citations.length; i++){
-        if(citations[i] <= i){
-            let answer = i
-            return answer
-        }
+  citations.sort((a, b) => b - a);
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] <= i) {
+      return i
     }
-    return citations.length;
+  }
+  return citations.length;
 }
