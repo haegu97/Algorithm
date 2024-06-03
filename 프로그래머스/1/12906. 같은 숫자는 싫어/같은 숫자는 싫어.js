c7bdc79333aa8b57683e1e32e7935bd4a23e2a70
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-06-03 18:47:39 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.12 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,8 @@
 function solution(arr)
 {
-    var answer = [];
+    let answer = [];
 
-    answer.push(arr[0])
-    
-    for(let i = 1; i < arr.length; i++){
+    for(let i = 0; i < arr.length; i++ ){
         if(arr[i] !== arr[i-1]){
             answer.push(arr[i])
         }
