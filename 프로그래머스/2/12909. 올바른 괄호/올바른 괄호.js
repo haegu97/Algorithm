c3983c20ec92982a9504824ca92b17761d2fccd1
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-06-10 20:49:38 +0900

    [level 2] Title: 올바른 괄호, Time: 2.61 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,10 +1,19 @@
 function solution(s){
-    let cnt = 0
+    let tmp = 0
     for(let i = 0; i < s.length; i++){
-        s[i] == "(" ? cnt++ : cnt -= 1
-            if(cnt < 0){
-            break
+        if(s[i] == "("){
+            tmp++
+        }else{
+            tmp -= 1
+        }
+        if(tmp == -1){
+            return false
         }
     }
-    return cnt == 0 ? true : false
+    if(tmp == 0){
+        return true
+    }else{
+        return false
+    }
+    
 }
