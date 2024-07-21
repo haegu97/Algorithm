653769a4a346002de17e4cb83bfdc8b1af592971
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-07-21 23:30:15 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,13 +1,10 @@
 function solution(phone_number) {
-    var answer = '';
-    let length = phone_number.length
-    for(let i = 0; i < length; i++){
-        if(i<length-4){
-            answer+= "*"
-        }
-        else{
-            answer+= phone_number[i]
-        }
+    let answer = ""
+    for(let i = 0; i < phone_number.length-4; i++){
+        answer += "*"
     }
-    return answer;
+    for(let i = phone_number.length-4; i < phone_number.length; i++){
+        answer += phone_number[i]
+    }
+    return answer
 }
