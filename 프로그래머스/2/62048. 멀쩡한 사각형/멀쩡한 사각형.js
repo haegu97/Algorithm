73164bf7b6cba0fc214b4fbdbf2fe9d76c28ced7
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-07-30 16:59:35 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 629.87 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.js
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.js
@@ -1,20 +1,11 @@
 function solution(w, h) {
-    if(w <= h){
-        let num = 0
-        for(let i = 1; i <= w; i++){
-            if(w % i == 0 && h % i == 0){
-                num = i
-            }
-        }
-        return (w * h) - ((w / num) + (h / num) - 1) * num
-        
+  const small = w > h ? w : h
+  
+    let num = 0;
+    for (let i = 1; i <= small; i++) {
+      if (w % i == 0 && h % i == 0) {
+        num = i;
+      }
     }
-    else{
-        for(let i = 1; i <= h; i++){
-            if(w % i == 0 && h % i == 0){
-                num = i
-            }
-        }
-        return (w * h) - ((w / num) + (h / num) - 1) * num
-    }
-}
+    return w * h - (w / num + h / num - 1) * num;
+  } 
