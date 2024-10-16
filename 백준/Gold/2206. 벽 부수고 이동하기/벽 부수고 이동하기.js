c666b60bfc7b7d8d6e3337c832ba6c6cac49aed0
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-10-16 16:20:19 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1976 ms, Memory: 347116 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -1,15 +1,21 @@
 let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
 let [N, M] = input[0].split(" ").map(Number);
 input = input.slice(1).map((v) => v.split("").map(Number));
-const visited = Array.from(new Array(N), () => new Array(M).fill(0).map(() => [0, 0])); // visited[y][x][isBreak]
+const visited = Array.from(new Array(N), () =>
+  new Array(M).fill(0).map(() => [0, 0])
+);
 const dx = [1, 0, -1, 0];
 const dy = [0, 1, 0, -1];
 const queue = [];
 
 queue.push([0, 0, 0]);
-visited[0][0][0] = 1; // 출발점 방문 처리
+visited[0][0][0] = 1;
 
 function BFS() {
   let idx = 0;
@@ -17,7 +23,6 @@ function BFS() {
   while (idx !== queue.length) {
     const [y, x, isBreak] = queue[idx];
 
-    // 도착점에 도달한 경우
     if (x === M - 1 && y === N - 1) {
       return visited[y][x][isBreak];
     }
@@ -25,16 +30,15 @@ function BFS() {
     for (let i = 0; i < dx.length; i++) {
       const [ny, nx] = [y + dy[i], x + dx[i]];
 
-      // 이동할 위치가 유효한 경우
       if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
         // 이동할 위치가 통로(0)인 경우
         if (input[ny][nx] === 0 && visited[ny][nx][isBreak] === 0) {
           visited[ny][nx][isBreak] = visited[y][x][isBreak] + 1; // 최단 경로 길이 업데이트
           queue.push([ny, nx, isBreak]);
-        } 
+        }
         // 이동할 위치가 벽(1)인 경우
         else if (input[ny][nx] === 1 && isBreak === 0) {
-          visited[ny][nx][isBreak + 1] = visited[y][x][isBreak] + 1; // 최단 경로 길이 업데이트
+          visited[ny][nx][1] = visited[y][x][isBreak] + 1; // 최단 경로 길이 업데이트
           queue.push([ny, nx, isBreak + 1]); // 벽을 부순 상태로 큐에 추가
         }
       }
