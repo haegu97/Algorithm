3b8f7d12861c540b27957b7b2838cfa4da660b89
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-10-22 16:13:34 +0900

    [Silver I] Title: 안전 영역, Time: 356 ms, Memory: 19520 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const N = Number(input.shift());
+const graph = input.map((line) => line.trim().split(" ").map(Number));
+
+let visited = Array.from({ length: N }, () => Array(N).fill(0));
+let answer = 1;
+
+let maxHeight = 0;
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    if (graph[i][j] > maxHeight) {
+      maxHeight = graph[i][j];
+    }
+  }
+}
+
+const bfs = (x, y, height) => {
+  const q = [[x, y]];
+  const dx = [-1, 1, 0, 0];
+  const dy = [0, 0, -1, 1];
+  while (q.length) {
+    const [x, y] = q.shift();
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+      if (
+        0 <= nx &&
+        nx < N &&
+        0 <= ny &&
+        ny < N &&
+        graph[nx][ny] > height &&
+        visited[nx][ny] == 0
+      ) {
+        q.push([nx, ny]);
+        visited[nx][ny] = 1;
+      }
+    }
+  }
+};
+
+const resetVisited = () => {
+  visited = Array.from({ length: N }, () => Array(N).fill(0));
+};
+
+for (let height = 1; height < maxHeight; height++) {
+  let cnt = 0;
+  resetVisited();
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (graph[i][j] > height && visited[i][j] == 0) {
+        bfs(i, j, height);
+        cnt++;
+      }
+    }
+  }
+  answer = Math.max(cnt, answer);
+}
+
+console.log(answer);
