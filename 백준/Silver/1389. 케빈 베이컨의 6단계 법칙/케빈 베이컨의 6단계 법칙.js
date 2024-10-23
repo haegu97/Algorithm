751228349cc44e2fec81f27b3c41f898301fc6b3
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-10-23 16:36:51 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 248 ms, Memory: 12168 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
@@ -7,40 +7,43 @@ const input = require('fs')
   .map((v) => v.split(' ').map(Number));
 const [N, M] = input.shift();
 const graph = Array.from(Array(N + 1), () => []);
-
-// 무방향(양방향) 그래프 만들기
-input.forEach(([from, to]) => {
-  graph[from].push(to);
-  graph[to].push(from);
+input.forEach(([start, end]) => {
+  graph[start].push(end);
+  graph[end].push(start);
 });
 
-const bfs = (start, target) => {
-  const queue = [[start, 0]];
-  const visited = Array(N + 1).fill(false);
+const bfs = (start, end) => {
+  const q = [[start, 0]];
+  const visited = Array(N + 1).fill(0);
 
-  while (queue.length) {
-    const [curVertax, distance] = queue.shift();
-    if (visited[curVertax]) continue; // 방문했으면 패스
-    visited[curVertax] = true; // 방문 안 했다면 방문 처리
-    if (curVertax == target) return distance; // 현재 정점 목표 정점에 도달하면 거리 반환
-    for (const nextVertax of graph[curVertax]) {
-      if (!visited[nextVertax]) {
-        queue.push([nextVertax, distance + 1]);
+  while (q.length) {
+    const [cur, distance] = q.shift();
+    if (visited[cur] === 1) {
+      continue;
+    }
+    visited[cur] = 1;
+
+    if (cur === end) {
+      return distance;
+    }
+
+    for (const next of graph[cur]) {
+      if (visited[next] === 0) {
+        q.push([next, distance + 1]);
       }
     }
   }
 };
 
-let result = []; // 사람 변호별로 케빈 베이컨의 수 담을 배열
-
-// 한 사람당 각각의 사람과의 관계의 거리를 알아야 하므로 반복문 중첩 
+const result = [];
 for (let i = 1; i <= N; i++) {
-  let sum = 0; // 케빈 베이컨의 수 담을 변수
+  let cnt = 0;
   for (let j = 1; j <= N; j++) {
-    sum += bfs(i, j); // 사람 관계 거리 누적합
+    const distance = bfs(i, j);
+    cnt += distance;
   }
-  result.push(sum);
+  result.push(cnt);
 }
 
-// 케빈 베이컨의 수가 가장 작은 사람의 번호를 구하고 해당 인덱스 값에 +1
-console.log(result.indexOf(Math.min(...result)) + 1);
+const minValue = Math.min(...result);
+console.log(result.indexOf(minValue) + 1);
