b62b6cfde98f190a306e21f2490ca9726d1bf2d1
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-11-13 08:19:23 +0900

    [Silver V] Title: 올림픽, Time: 160 ms, Memory: 10140 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.js
+++ 백준/Silver/8979. 올림픽/올림픽.js
@@ -6,21 +6,18 @@ let input = fs
   .trim()
   .split("\n");
 
-const [N, K] = input
-  .shift()
-  .split(" ")
-  .map((v) => Number(v));
+const [N, K] = input.shift().split(" ").map(Number);
 
-input = input.map((line) => line.split(" ").map((v) => Number(v)));
+input = input.map((line) => line.split(" ").map(Number));
 
 input.sort((a, b) => {
-  if (a[1] !== b[1]) return b[1] - a[1];
-  else if (a[2] !== b[2]) return b[2] - a[2];
-  else return a[3] - b[3];
+  if (b[1] !== a[1]) return b[1] - a[1];
+  else if (b[2] !== a[2]) return b[2] - a[2];
+  else return b[3] - a[3];
 });
 
-let same = 1;
-input[0][4] = 1;
+let rank = 1;
+input[0][4] = rank;
 
 for (let i = 1; i < N; i++) {
   if (
@@ -28,11 +25,10 @@ for (let i = 1; i < N; i++) {
     input[i][2] === input[i - 1][2] &&
     input[i][3] === input[i - 1][3]
   ) {
-    input[i][4] = input[i - 1][4];
-    same++;
+    input[i][4] = rank;
   } else {
-    input[i][4] = input[i - 1][4] + same;
-    same = 1;
+    rank = i + 1;
+    input[i][4] = rank;
   }
 }
 
