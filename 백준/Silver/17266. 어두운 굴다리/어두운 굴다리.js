f60a38f748265bfd5f6fdafdc7eea3cc477c3c11
Sungu Kim <108677235+haegu97@users.noreply.github.com>
2024-11-22 10:00:11 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 176 ms, Memory: 17312 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
@@ -5,33 +5,33 @@ let input = fs
   .trim()
   .split("\n");
 
-const N = Number(input[0]);
-const M = Number(input[1]);
-const li = [0, ...input[2].split(" ").map(Number), N];
+const tunnelLength = Number(input[0]);
+const lightCnt = Number(input[1]);
+const lights = input[2].split(" ").map(Number);
 
-function bs(li, m) {
-  if (li[1] - li[0] > m) return false;
-  if (li[li.length - 1] - li[li.length - 2] > m) return false;
+function isValid(lights, height) {
+  if (lights[0] > height) return false;
+  if (tunnelLength - lights[lightCnt - 1] > height) return false;
 
-  for (let i = 1; i < li.length - 2; i++) {
-    if ((li[i + 1] - li[i]) / 2 > m) return false;
+  for (let i = 0; i < lights.length - 1; i++) {
+    if ((lights[i + 1] - lights[i]) / 2 > height) return false;
   }
 
   return true;
 }
 
 let s = 0;
-let e = N;
+let e = tunnelLength;
 let res = 0;
 
 while (s <= e) {
-  const m = Math.floor((s + e) / 2);
+  const mid = Math.floor((s + e) / 2);
 
-  if (bs(li, m)) {
-    e = m - 1;
-    res = m;
+  if (isValid(lights, mid)) {
+    e = mid - 1;
+    res = mid;
   } else {
-    s = m + 1;
+    s = mid + 1;
   }
 }
 
